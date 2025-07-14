be9dd5e3d69d8a8459f18a38829ef38d817bbb20
Camila Hinterholz <hinterholz.fotografia@gmail.com>
2025-07-14 11:11:19 -0300

    Corrige importação do App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'  // <- caminho relativo ao main.jsx
+import App from './App.jsx'  // <- caminho relativo ao main.jsx
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
